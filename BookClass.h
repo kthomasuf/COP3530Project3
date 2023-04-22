1810e913781e8945321c387ed1eec1a475477186
Kenny Thomas <kenny@Kennys-MacBook-Pro.local>
2023-04-22 15:20:26 -0400

    Update

--- BookClass.h
+++ BookClass.h
@@ -1,16 +1,23 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <set>
 
 class BookClass {
 private:
+    std::string allGenres;
     std::string fullTitle;
     std::string numberOfReviews;
     int numberOfReviewsInt;
     std::vector<std::string> uniqueWordsVector;
+    std::vector<std::string> uniqueGenresVector;
+    std::set<std::string> genreSet;
+
     void uniqueWords(std::string title);
+    void uniqueGenres(std::string genre);
 public:
-    BookClass(std::string title, std::string totalRatings);
+    BookClass(std::string genre, std::string title, std::string totalRatings);
     std::vector<std::string> returnUniqueWords();
+    std::set<std::string> returnGenres();
     int returnNumberOfReviews();
 };
